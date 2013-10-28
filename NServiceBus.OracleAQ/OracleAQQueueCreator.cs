c85556741faaec59d1468baef59788500147fce0
rosieks <rosieks@hotmail.com>
2013-10-28 19:24:54 +0100

    Improved queue creation

--- NServiceBus.OracleAQ/OracleAQQueueCreator.cs
+++ NServiceBus.OracleAQ/OracleAQQueueCreator.cs
@@ -1,5 +1,7 @@
 ﻿namespace NServiceBus.Transports.OracleAQ
 {
+    using System;
+    using NServiceBus.Logging;
     using Oracle.DataAccess.Client;
 
     /// <summary>
@@ -7,9 +9,9 @@
     /// </summary>
     public class OracleAQQueueCreator : ICreateQueues
     {
-        private const string CreateQueue = @"
+        private const string CreateQueueSql = @"
             DECLARE 
-                cnt NUMBER; 
+                cnt NUMBER;
             BEGIN 
                 SELECT count(*) INTO cnt FROM dba_queues WHERE name = :queue;
                 IF cnt = 0 THEN
@@ -21,7 +23,7 @@
                 DBMS_AQADM.START_QUEUE(:queue);
             END;";
 
-        private const string CreateQueueTable = @"
+        private const string CreateQueueTableSql = @"
             DECLARE 
                 cnt NUMBER; 
             BEGIN 
@@ -30,6 +32,10 @@
                     dbms_aqadm.create_queue_table(:queueTable, 'SYS.XMLType');
                 END IF;
             END;";
+
+        private const string DoesQueueExistSql = @"SELECT count(*) FROM dba_queues WHERE name = :queue";
+
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(OracleAQQueueCreator));
 
         public string ConnectionString { get; set; }
 
@@ -42,6 +48,52 @@
         /// <param name="account">Not used parameter</param>
         public void CreateQueueIfNecessary(Address address, string account)
         {
+            Logger.DebugFormat("Checking if queue exists: {0}.", address);
+
+            try
+            {
+                if (!this.DoesQueueExist(address))
+                {
+                    Logger.WarnFormat("Queue {0} does not exist.", address);
+                    Logger.DebugFormat("Going to create queue table: {0}", address);
+
+                    this.CreateQueueTable(address);
+                    this.CreateQueue(address);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(
+                    string.Format("Could not create queue {0} or check its existence. Processing will still continue.", address),
+                    ex);
+            }
+        }
+
+        private void CreateQueue(Address address)
+        {
+            string queue = this.NamePolicy.GetQueueName(address);
+            string queueTable = this.NamePolicy.GetQueueTableName(address);
+
+            using (OracleConnection conn = new OracleConnection(this.ConnectionString))
+            {
+                conn.Open();
+                using (OracleCommand createQueue = conn.CreateCommand())
+                {
+                    createQueue.BindByName = true;
+                    createQueue.CommandText = CreateQueueSql;
+                    createQueue.Parameters.Add("queue", queue);
+                    createQueue.Parameters.Add("queueTable", queueTable);
+                    createQueue.ExecuteNonQuery();
+                }
+
+                Logger.DebugFormat("Created queue, name: [{0}], queue table: [{1}]", queue, queueTable);
+            }
+        }
+
+        private void CreateQueueTable(Address address)
+        {
+            string queueTable = this.NamePolicy.GetQueueTableName(address);
+
             using (OracleConnection conn = new OracleConnection(this.ConnectionString))
             {
                 conn.Open();
@@ -49,18 +101,27 @@
                 using (OracleCommand createTable = conn.CreateCommand())
                 {
                     createTable.BindByName = true;
-                    createTable.CommandText = CreateQueueTable;
-                    createTable.Parameters.Add("queueTable", this.NamePolicy.GetQueueTableName(address));
+                    createTable.CommandText = CreateQueueTableSql;
+                    createTable.Parameters.Add("queueTable", queueTable);
                     createTable.ExecuteNonQuery();
                 }
 
-                using (OracleCommand createQueue = conn.CreateCommand())
+                Logger.DebugFormat("Created queue table: [{0}]", queueTable);
+            }
+        }
+
+        private bool DoesQueueExist(Address address)
+        {
+            using (OracleConnection conn = new OracleConnection(this.ConnectionString))
+            {
+                conn.Open();
+
+                using (OracleCommand createTable = conn.CreateCommand())
                 {
-                    createQueue.BindByName = true;
-                    createQueue.CommandText = CreateQueue;
-                    createQueue.Parameters.Add("queue", this.NamePolicy.GetQueueName(address));
-                    createQueue.Parameters.Add("queueTable", this.NamePolicy.GetQueueTableName(address));
-                    createQueue.ExecuteNonQuery();
+                    createTable.BindByName = true;
+                    createTable.CommandText = DoesQueueExistSql;
+                    createTable.Parameters.Add("queue", this.NamePolicy.GetQueueName(address));
+                    return Convert.ToBoolean(createTable.ExecuteScalar());
                 }
             }
         }
