a3acb0bb56a72f3ad1f67a7da871f2dc7ee9e5c0
rosieks <rosieks@hotmail.com>
2014-07-14 18:51:58 +0200

    OracleStorageContext

---
+++ NServiceBus.OracleAQ/OracleStorageContext.cs
@@ -0,0 +1,37 @@
+﻿namespace NServiceBus.Transports.OracleAQ
+{
+    using System.Data;
+    using NServiceBus.Pipeline;
+
+    /// <summary>
+    /// Provides users with access to the current Oracle transport <see cref="IDbConnection"/>. 
+    /// </summary>
+    public class OracleStorageContext
+    {
+        private readonly string connectionString;
+        private readonly PipelineExecutor pipelineExecutor;
+
+        public OracleStorageContext(PipelineExecutor pipelineExecutor, string connectionString)
+        {
+            this.pipelineExecutor = pipelineExecutor;
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Gets the current context Oracle transport <see cref="IDbConnection"/> or <code>null</code> if no current context Oracle transport <see cref="IDbConnection"/> available.
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get
+            {
+                IDbConnection connection;
+                if (this.pipelineExecutor.CurrentContext.TryGet(string.Format("SqlConnection-{0}", this.connectionString), out connection))
+                {
+                    return connection;
+                }
+
+                return null;
+            }
+        }
+    }
+}
